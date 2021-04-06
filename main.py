e75cccee5b5458c2f9e0f68cdd6f9d2eb905f56e
Nordic Games <69092549+DatNordicGames@users.noreply.github.com>
2021-04-06 19:55:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,7 @@ import json
 from replit import db
 from time import sleep
 
-from flask import Flask
-from threading import Thread
-app = Flask('')
-@app.route('/')
-def main():
-  return "Your Bot Is Ready"
-def run():
-  app.run(host="0.0.0.0", port=8000)
-def keep_alive():
-  server = Thread(target=run)
-  server.start()
+
 
 client = discord.Client()
 
